ed7ffc8152dac11faa1acc05d056d7a78bfaecb8
Elis <soujirofb@gmail.com>
2025-08-24 14:04:27 +0800

    將Log 改為Serilog
    將Log 改為Serilog
    Log寫入TXT檔案

--- InterViewProject/Program.cs
+++ InterViewProject/Program.cs
@@ -1,27 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Starting web host");
+
+    var builder = WebApplication.CreateBuilder(args);
+   
+    Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration) // 從appsettings.json 讀取Logger設定
+    .CreateLogger();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Host.UseSerilog();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
+    return 0;
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
